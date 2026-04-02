7831b3bf8e103ee15b6778e8b3d8973ca2976ad0
shyaar <shyaarsati@gmail.com>
2026-04-02 20:15:32 +0100

    Generics

--- src/lesson2.rs
+++ src/lesson2.rs
@@ -25,3 +25,45 @@ fn main() {
 
     println!("You can watch this movie, adults only, {age} is old enough")
 }
+
+
+
+#[derive(Debug)]
+enum Tier{
+    Gold,
+    Silver,
+    Platinum,
+}
+
+#[derive(Debug)]
+enum Subscription{
+    Free,
+    Basic(f64, u32),
+    Premium{tier: Tier}
+}
+
+impl Subscription{
+    fn summarize(&self){
+        match self{
+            Subscription::Free => println!("You have limited access to the site"),
+
+            Subscription::Basic(price, duration) => println!("You have limited access to the site's premium features for {price} for {duration} months"),
+
+            Subscription::Premium{tier} => println!("You have full access to the site's premium features. Your tier is {tier:?}"),
+        }
+    }
+}
+
+
+fn main(){
+    let free = Subscription::Free;
+    free.summarize();
+
+    let basic = Subscription::Basic(2_500.00, 4);
+    basic.summarize();
+
+    let premium = Subscription::Premium{tier: Tier::Platinum};
+    premium.summarize();
+
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,12 @@
 
-#[derive(Debug)]
-enum Tier{
-    Gold,
-    Silver,
-    Platinum,
+
+fn main() {
+    println!("{}", user(2));
+    println!("{}", user(true));
+    println!("{}", user("John"));
+    println!("{}", user(String::from("Andrew")));
 }
 
-#[derive(Debug)]
-enum Subscription{
-    Free,
-    Basic(f64, u32),
-    Premium{tier: Tier}
-}
-
-impl Subscription{
-    fn summarize(&self){
-        match self{
-            Subscription::Free => println!("You have limited access to the site"),
-
-            Subscription::Basic(price, duration) => println!("You have limited access to the site's premium features for {price} for {duration} months"),
-
-            Subscription::Premium{tier} => println!("You have full access to the site's premium features. Your tier is {tier:?}"),
-        }
-    }
-}
-
-
-fn main(){
-    let free = Subscription::Free;
-    free.summarize();
-
-    let basic = Subscription::Basic(2_500.00, 4);
-    basic.summarize();
-
-    let premium = Subscription::Premium{tier: Tier::Platinum};
-    premium.summarize();
-
-    
+fn user<T>(name: T) -> T {
+    name
 }
