454dacfe8e4fcd034c04d2fa6a2504a836152b29
shyaar <shyaarsati@gmail.com>
2026-04-09 21:30:27 +0100

    return option

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,51 @@
-fn main(){
-    let musical_instruments: [String;3]= [
+fn main() {
+    let musical_instruments: [String; 3] = [
         String::from("guitar"),
         String::from("piano"),
         String::from("drums"),
     ];
 
-
     let piano = musical_instruments.get(1);
     println!("{:?}", piano);
 
-    let invalid = musical_instruments.get(100);
+    // let invalid = musical_instruments.get(100);
 
+    // match piano {
+    //     Option::Some(musical_instrument) => println!("{:?}", musical_instrument),
+    //     Option::None => println!("No instrument found"),
+    // }
 
-    match piano{
-        Option::Some(musical_instrument) => println!("{:?}", musical_instrument),
+    // match invalid {
+    //     Option::Some(musical_instrument) => println!("{:?}", musical_instrument),
+    //     Option::None => println!("No instrument found"),
+    // }
+
+    play(piano);
+
+    let available: Option <bool> = is_in_stock(true,true);
+    println!("{:?}", available);
+
+    match available{
+        Option::Some(true) => println!("item is Available"),
+        Option::Some(false) => println!("item is not Available"),
+        Option::None => println!("item is not in the system"),
+    }
+}
+
+fn play(instrument: Option<&String>) {
+    match instrument {
+        Option::Some(inst) => println!("playing {inst}"),
         Option::None => println!("No instrument found"),
     }
+}
 
-    match invalid{
-        Option::Some(musical_instrument) => println!("{:?}", musical_instrument),
-        Option::None => println!("No instrument found"),
+
+fn is_in_stock(in_system:bool, in_stock:bool)->Option<bool>{
+    if in_system && in_stock{
+        Option::Some(true)
+    }else if in_system{
+        Option::Some(false)
+    }else{
+        Option::None
     }
 }
